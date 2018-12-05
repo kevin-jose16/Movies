386f95cfe565a1004b7e3e5ceafa551b75c17edc
dario <dprolo@humana.com>
2018-12-05 09:31:14 -0300

    first commit

---
+++ Movies/Clients.swift
@@ -0,0 +1,9 @@
+//
+//  Clients.swift
+//  Movies
+//
+//  Created by Dario on 4/12/18.
+//  Copyright © 2018 Dario. All rights reserved.
+//
+
+import Foundation
---
+++ Movies/MovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieViewController.swift
+//  Movies
+//
+//  Created by Dario on 3/12/18.
+//  Copyright © 2018 Dario. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
